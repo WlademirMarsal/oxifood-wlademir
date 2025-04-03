80d0d4e65163a47825d6c2c36a8adb63f6cf331a
aluno <aluno@ifpe>
2025-04-02 21:11:08 -0300

    criação e configuração no projeto

---
+++ src/main/java/br/com/ifpe/oxifood/OxifoodWlademirApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxifoodWlademirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxifoodWlademirApplication.class, args);
+	}
+
+}
