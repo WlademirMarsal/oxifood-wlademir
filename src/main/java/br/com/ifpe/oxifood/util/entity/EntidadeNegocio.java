e675a858c89894585cfc63aabbc5f4670d4dc87a
Arthur <arthur@gmail.com>
2025-04-09 19:34:34 -0300

    enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxifood/util/entity/EntidadeNegocio.java
@@ -0,0 +1,20 @@
+package br.com.ifpe.oxifood.util.entity;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+public abstract class EntidadeNegocio implements Serializable {
+
+    private Long id;
+
+    private Boolean habilitado;
+    
+}
+
